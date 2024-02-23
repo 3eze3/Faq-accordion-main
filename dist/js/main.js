f27c39b4b1f7a36f1b53ea8f504d2526cc26e088
Ezequiel Cordova <ezekics33@gmail.com>
2024-02-22 21:31:42 -0500

    Add the function to interactive card

---
+++ dist/js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+const $head = document.querySelectorAll(".accordion__head");
+const $icons = document.querySelectorAll(".accordion__icon");
+const $description = document.querySelectorAll(".accordion__description");
+const iconSrcActive = "../dist/assets/images/icon-minus.svg";
+const iconSrc = "../dist/assets/images/icon-plus.svg";
+$head.forEach((head, index) => {
+    head.addEventListener("click", () => {
+        const isActive = $description[index].classList.contains("accordion__description--active");
+        $description.forEach(content => {
+            content.classList.remove("accordion__description--active");
+        });
+        $icons.forEach((icon) => {
+            icon.src = iconSrc;
+        });
+        if (!isActive) {
+            $icons[index].src = iconSrcActive;
+            $description[index].classList.add("accordion__description--active");
+        }
+    });
+});
